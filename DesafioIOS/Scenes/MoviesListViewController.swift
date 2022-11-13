7c7a98e0d1a8d7a6acfa9ced90c807bd6c1482a5
Emerson <emerson.carpes@gmail.com>
2022-11-13 09:52:20 -0300

    Initial setup for view code

---
+++ DesafioIOS/Scenes/MoviesListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesListViewController.swift
+//  DesafioIOS
+//
+//  Created by Emerson Carpes on 13/11/22.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController {
+    
+    weak var coordinator: MoviesListCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
